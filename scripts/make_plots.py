0977533b799a4e97decd745e32d3e5300ad0c1e5
Alasdair Tran <alasdair.tran@gmail.com>
2020-01-18 16:06:10 +1100

    Update plots

--- scripts/make_plots.py
+++ scripts/make_plots.py
@@ -14,23 +14,34 @@ def make_boxplots():
     with open('expt/2_naive_seasonal/serialization/evaluate-metrics.json') as f:
         smape['SN'] = json.load(f)['smape']
 
-    with open('expt/7_lstm/serialization/evaluate-metrics.json') as f:
+    with open('expt/11_no_agg/serialization/evaluate-metrics.json') as f:
         smape['LSTM'] = json.load(f)['smape']
 
-    smapes = [smape['naive'], smape['SN'], smape['LSTM']]
+    with open('expt/9_agg_mean/serialization/evaluate-metrics.json') as f:
+        smape['mean'] = json.load(f)['smape']
 
-    fig = plt.figure(figsize=(5, 4))
+    with open('expt/8_agg_attention/serialization/evaluate-metrics.json') as f:
+        smape['attn'] = json.load(f)['smape']
+
+    with open('expt/10_gcn/serialization/evaluate-metrics.json') as f:
+        smape['gcn'] = json.load(f)['smape']
+
+    smapes = [smape['naive'], smape['SN'], smape['LSTM'],
+              smape['mean'], smape['attn'], smape['gcn']]
+
+    fig = plt.figure(figsize=(8, 6))
     ax = plt.subplot(1, 1, 1)
     ax.boxplot(smapes, showfliers=False, meanline=True,
                showmeans=True, widths=0.7)
-    ax.set_xticklabels(['Naive', 'Seasonal Naive', 'Shared LSTM'])
+    ax.set_xticklabels(
+        ['Naive', 'Seasonal', 'No Agg', 'Mean', 'Attn', 'GCN'])
     ax.set_ylabel('SMAPE')
 
     means = [np.mean(x) for x in smapes]
     pos = range(len(means))
     for tick, label in zip(pos, ax.get_xticklabels()):
         ax.text(pos[tick] + 0.85, means[tick] +
-                0.2, '{0:.3f}'.format(means[tick]))
+                0.07, '{0:.3f}'.format(means[tick]))
 
     fig.tight_layout()
     plt.show()
