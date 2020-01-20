9e3d52c0b365604276171cd57ce986bc057f6298
Alasdair Tran <alasdair.tran@gmail.com>
2020-01-20 12:57:11 +1100

    Use daily snapshots

--- nos/models/__init__.py
+++ nos/models/__init__.py
@@ -1,5 +1,6 @@
 from .lstm import TimeSeriesLSTM
 from .lstm_network import TimeSeriesLSTMNetwork
+from .lstm_network_daily import TimeSeriesLSTMNetworkDaily
 from .naive import (NaivePreviousDayModel, NaiveRollingAverageModel,
                     NaiveSeasonalModel)
 from .transformer import TimeSeriesTransformer
