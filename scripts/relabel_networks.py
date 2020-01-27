f15a5f04f11d4cf52160c373306d003b3c3f26ec
Alasdair Tran <alasdair.tran@gmail.com>
2020-01-27 18:00:21 +1100

    Train with video tags

--- scripts/relabel_networks.py
+++ scripts/relabel_networks.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 from typing import Dict, List
 
 import pandas as pd
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -31,7 +32,7 @@ def relabel_networks():
 
     logger.info('Remapping node IDs.')
     sources: Dict[int, List[int]] = defaultdict(list)
-    for _, row in network_df.iterrows():
+    for _, row in tqdm(network_df.iterrows()):
         target = int(row['target'])
         source = int(row['source'])
         sources[target].append(source)
@@ -47,10 +48,12 @@ def relabel_networks():
     full_series: Dict[int, List[int]] = {}
     series: Dict[int, List[int]] = {}
     path = os.path.join(data_dir, 'vevo_forecast_data_60k.tsv')
+    embed_dict = {}
     with open(path) as f:
-        for line in f:
-            embed, _, ts_view, _ = line.rstrip().split('\t')
+        for line in tqdm(f):
+            embed, embed_name, ts_view, _ = line.rstrip().split('\t')
             key = int(embed)
+            embed_dict[embed_name] = key
             full_series[key] = [int(x) for x in ts_view.split(',')]
             if int(embed) in target_ids:
                 key = int(embed)
@@ -67,23 +70,51 @@ def relabel_networks():
 
     snapshots = defaultdict(dict)
 
+    # TODO: COMMENTED OUT FOR FASTER LOADING. UNCOMMENT THIS WHEN CODE IS PUBLISHED
     # Daily snapshots
-    start = datetime(2018, 9, 1)
-    for i in range(63):
-        d = start + timedelta(days=i)
-        filename = f'network_{d.year}-{d.month:02}-{d.day:02}.p'
-        path = os.path.join(data_dir, 'network_pickle', filename)
-        with open(path, 'rb') as f:
-            obj = pickle.load(f)
+    # start = datetime(2018, 9, 1)
+    # for i in tqdm(range(63)):
+    #     d = start + timedelta(days=i)
+    #     filename = f'network_{d.year}-{d.month:02}-{d.day:02}.p'
+    #     path = os.path.join(data_dir, 'network_pickle', filename)
+    #     with open(path, 'rb') as f:
+    #         obj = pickle.load(f)
 
-        for key, values in obj.items():
-            values = sorted(values, key=lambda x: x[2], reverse=True)
-            sources = [v[0] for v in values]
-            snapshots[i][key] = sources
+    #     for key, values in obj.items():
+    #         values = sorted(values, key=lambda x: x[2], reverse=True)
+    #         sources = [v[0] for v in values]
+    #         snapshots[i][key] = sources
 
-    snapshot_path = os.path.join(data_dir, 'snapshots.json')
-    with open(snapshot_path, 'w') as f:
-        json.dump(snapshots, f)
+    # snapshot_path = os.path.join(data_dir, 'snapshots.json')
+    # with open(snapshot_path, 'w') as f:
+    #     json.dump(snapshots, f)
+
+    tags = ['acoustic', 'alternative', 'audio', 'blue', 'classical', 'country',
+            'cover', 'dance', 'electronic', 'gospel', 'guitar', 'hd', 'hip hop',
+            'holiday', 'indie', 'instrumental', 'jazz', 'karaoke', 'live',
+            'lyrics', 'metal', 'musical', 'official', 'piano', 'pop', 'r&b',
+            'rap', 'remix', 'rock', 'single']
+
+    path = 'data/vevo_en_videos_60k.json'
+    vevo_tags = {}
+    with open(path) as f:
+        for line in tqdm(f):
+            o = json.loads(line)
+            key = embed_dict[o['id']]
+            cleaned_tags = set()
+            if 'tags' in o['snippet']:
+                v_tags = o['snippet']['tags']
+                for t in v_tags:
+                    t = t.lower().replace('-', ' ')
+                    for tag in tags:
+                        if tag in t:
+                            cleaned_tags.add(tag)
+            cleaned_tags = sorted(cleaned_tags)
+            vevo_tags[key] = cleaned_tags
+
+    vevo_tag_path = os.path.join(data_dir, 'vevo_tags.json')
+    with open(vevo_tag_path, 'w') as f:
+        json.dump(vevo_tags, f)
 
 
 def main():
