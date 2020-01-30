84d55e31b1b887d0099a441f13fd26cfb3183e98
Alasdair Tran <alasdair.tran@gmail.com>
2020-01-30 16:42:46 +1100

    Use only neighbouring info in current timestep

--- nos/models/__init__.py
+++ nos/models/__init__.py
@@ -3,6 +3,7 @@ from .lstm_network import TimeSeriesLSTMNetwork
 from .lstm_network_daily import TimeSeriesLSTMNetworkDaily
 from .lstm_network_residual import TimeSeriesLSTMNetworkResidual
 from .lstm_network_tags import TimeSeriesLSTMNetworkTags
+from .lstm_network_weighted import TimeSeriesLSTMNetworkWeighted
 from .naive import (NaivePreviousDayModel, NaiveRollingAverageModel,
                     NaiveSeasonalDiffModel, NaiveSeasonalModel)
 from .transformer import TimeSeriesTransformer
