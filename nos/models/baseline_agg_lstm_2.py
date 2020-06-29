eb153f0acc703256c7d7b7616ec30c8371fa7a53
Alasdair Tran <alasdair.tran@gmail.com>
2020-06-29 21:34:10 +1000

    Remove _populate_cache

--- nos/models/baseline_agg_lstm_2.py
+++ nos/models/baseline_agg_lstm_2.py
@@ -129,18 +129,12 @@ class BaselineAggLSTM2(BaseModel):
 
         return X
 
-    def _get_neighbour_embeds(self, X, keys, n_skips=None, forward_full=False,
-                              use_cache=False, use_gt_day=None):
+    def _get_neighbour_embeds(self, X, keys, n_skips=None, forward_full=False):
         if self.agg_type == 'none':
             return X
 
         neighbor_lens = []
         source_list = []
-
-        if use_gt_day is not None and self.peek:
-            use_gt_day += 1
-        elif self.peek and n_skips is not None and n_skips > 0:
-            n_skips -= 1
 
         for key in keys:
             if key in self.sources:
@@ -149,38 +143,20 @@ class BaselineAggLSTM2(BaseModel):
                 sources = []
             neighbor_lens.append(len(sources))
             for s in sources:
-                if use_gt_day is not None:
-                    s_series = self.series[s]
-                    cutoff = - \
-                        (self.n_days-use_gt_day) if use_gt_day < 7 else None
-                    s_series = s_series[:cutoff]
-                elif use_cache:
-                    s_series = self.cached_series[s]
-                else:
-                    s_series = self.series[s]
-                    s_series = s_series[:-n_skips if n_skips > 0 else None]
+                s_series = self.series[s]
+                s_series = s_series[:-n_skips if n_skips > 0 else None]
                 source_list.append(s_series)
 
         sources = torch.stack(source_list, dim=0)
         # sources.shape == [batch_size * n_neighbors, seq_len]
 
-        if not forward_full and not self.peek:
+        if not forward_full:
             X_neighbors, _ = self._forward(sources)
-        elif not forward_full and self.peek and n_skips == 0:
-            X_neighbors = self._forward_full(sources)
-            X_neighbors = X_neighbors[:, 1:]
-        elif not forward_full and self.peek:
-            X_neighbors, _ = self._forward(sources)
-            X_neighbors = X_neighbors[:, 1:]
-        elif forward_full and self.peek:
-            X_neighbors = self._forward_full(sources)
-            X_neighbors = X_neighbors[:, 1:]
         else:
             X_neighbors = self._forward_full(sources)
-        # X_neighbors.shape == [batch_size * n_neighbors, seq_len, hidden_size]
+            X_neighbors = X_neighbors[:, 1:]
 
-        if X.shape[1] == 1:
-            X_neighbors = X_neighbors[:, -1:]
+        # X_neighbors.shape == [batch_size * n_neighbors, seq_len, hidden_size]
 
         # Go through each element in the batch
         cursor = 0
@@ -275,15 +251,29 @@ class BaselineAggLSTM2(BaseModel):
 
         # During evaluation, we compute one time step at a time
         if splits[0] in ['test']:
-            self._populate_cache(splits)
-
-            pred_list, target_list = [], []
+            target_list = []
             for key in keys:
-                pred_list.append(self.cached_series[key][-self.n_days:])
-                target_list.append(self.series[key][-self.n_days:])
-
-            preds = torch.stack(pred_list, dim=0)
+                target_list.append(self.series[key][-self.forecast_length:])
             targets = torch.stack(target_list, dim=0)
+            # targets.shape == [batch_size, forecast_len]
+
+            preds = targets.new_zeros(*targets.shape)
+
+            series = series[:, :-self.forecast_length]
+            current_views = series[:, -1]
+            for i in range(self.forecast_length):
+                X = self._forward_full(series)
+                X_full = self._get_neighbour_embeds(
+                    X, keys, self.forecast_length - i - 1, True)
+                X_full = self.fc(X_full)
+                delta = X_full.squeeze(-1)[:, -1]
+                # delta.shape == [batch_size]
+
+                current_views = current_views * delta
+                preds[:, i] = current_views
+                series = torch.cat(
+                    [series, current_views.unsqueeze(-1)], dim=-1)
+
             smape, daily_errors = get_smape(targets, preds)
 
             out_dict['smapes'] = smape
@@ -291,78 +281,6 @@ class BaselineAggLSTM2(BaseModel):
             out_dict['keys'] = keys
 
         return out_dict
-
-    def _populate_cache(self, splits):
-        if self.cached_series:
-            return
-
-        hidden_dict = {}
-        n_skips = self.n_days
-
-        # Remove the final n_skips days from the input series since they are
-        # what we want to predict. As we evaluate, series_dict will
-        # store the entire series including the latest predictions.
-        for key in self.series.keys():
-            s = self.series[key]
-            s = s[:-n_skips]
-            self.cached_series[key] = s
-
-        batch_size = 512
-        keys = sorted(self.series.keys())
-
-        for day in range(self.n_days):
-            logger.info(f'Populating cache day {day}')
-
-            # First pass: Get the latest hidden state of all nodes with an edge
-            # (incoming or outgoing) before taking the network effect into
-            # account. We do this in batch of 512
-            for i in range(0, len(keys), batch_size):
-                batch_keys = keys[i:i+batch_size]
-
-                series_list = []
-                for key in batch_keys:
-                    s = self.cached_series[key]
-                    series_list.append(s)
-                series = torch.stack(series_list, dim=0)
-
-                hidden = self._forward_full(series)
-                # hidden.shape == [batch_size, seq_len, hidden_size]
-
-                # We store the hidden state in the latest timestep (the
-                # step that we are predicting in hidden_dict
-                for key, h in zip(batch_keys, hidden):
-                    hidden_dict[key] = h[-1:]
-                    # hidden_dict[key].shape == [1, hidden_size]
-
-            # Second pass: aggregate effect from neighboring nodes
-            new_cached_series = {}
-            for i in range(0, len(keys), batch_size):
-                batch_keys = keys[i:i+batch_size]
-
-                hidden_list = []
-                for key in batch_keys:
-                    h = hidden_dict[key]
-                    hidden_list.append(h)
-                X = torch.stack(hidden_list, dim=0)
-                # X.shape == [batch_size, 1, hidden_size]
-
-                X_full = self._get_neighbour_embeds(
-                    X, batch_keys, forward_full=True, use_cache=True, use_gt_day=day)
-                # X_full.shape == [batch_size, 1, hidden_size]
-
-                # This is our prediction, the percentage change from
-                # the previous time step.
-                pct = self.fc(X_full).squeeze(2).squeeze(1)
-                # pct.shape == [batch_size]
-
-                # Calculate the predicted view count
-                for i, key in enumerate(batch_keys):
-                    pred = self.cached_series[key][-1:] * pct[i]
-
-                    new_cached_series[key] = torch.cat(
-                        [self.cached_series[key], pred], dim=0)
-
-            self.cached_series = new_cached_series
 
     @classmethod
     def get_params(cls, vocab: Vocabulary, params: Params) -> Dict[str, Any]:
