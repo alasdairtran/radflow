0a9d9da6edd1a2d967a416ec9cdc80da6a1bdcda
Alasdair Tran <alasdair.tran@gmail.com>
2020-07-23 20:57:22 +1000

    Remove NaiveWiki model

--- nos/models/__init__.py
+++ nos/models/__init__.py
@@ -1,4 +1,4 @@
 from .baseline_agg_lstm_2 import BaselineAggLSTM2
 from .baseline_agg_lstm_4 import BaselineAggLSTM4, NewNaive
-from .nbeats_wiki import NaiveWiki, NBEATSWiki
+from .nbeats_wiki import NBEATSWiki
 from .nbeats_wiki_transformer_bit import NBEATSTransformerBit
