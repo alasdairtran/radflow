7f962dbc80be680d539b252a823e753185042d01
Alasdair Tran <alasdair.tran@gmail.com>
2020-07-23 14:34:19 +1000

    Delete old models

--- nos/models/__init__.py
+++ nos/models/__init__.py
@@ -1,26 +1,4 @@
-from .baseline_agg_lstm import BaselineAggLSTM
 from .baseline_agg_lstm_2 import BaselineAggLSTM2
-from .baseline_agg_lstm_3 import BaselineAggLSTM3
 from .baseline_agg_lstm_4 import BaselineAggLSTM4, NewNaive
-from .lstm import TimeSeriesLSTM
-from .lstm_network import TimeSeriesLSTMNetwork
-from .lstm_network_daily import TimeSeriesLSTMNetworkDaily
-from .lstm_network_layered import TimeSeriesLSTMNetworkDailyLayered
-from .lstm_network_log import TimeSeriesLSTMNetworkLog
-from .lstm_network_log_diff import TimeSeriesLSTMNetworkLogDiff
-from .lstm_network_p1 import TimeSeriesLSTMNetworkP1
-from .lstm_network_pct import TimeSeriesLSTMNetworkPCT
-from .lstm_network_residual import TimeSeriesLSTMNetworkResidual
-from .lstm_network_tags import TimeSeriesLSTMNetworkTags
-from .lstm_network_weighted import TimeSeriesLSTMNetworkWeighted
-from .lstm_network_wiki import TimeSeriesLSTMNetworkWiki
-from .naive import (NaivePreviousDayModel, NaiveRollingAverageModel,
-                    NaiveSeasonalDiffModel, NaiveSeasonalModel)
-from .nbeats_lstm import NBEATSLSTM
-from .nbeats_lstm_baseline import NBEATSLSTMBaseline
 from .nbeats_wiki import NaiveWiki, NBEATSWiki
-from .nbeats_wiki_transformer import NBEATSTransformer
 from .nbeats_wiki_transformer_bit import NBEATSTransformerBit
-from .nbeats_wiki_transformer_peek import NBEATSTransformerPeek
-from .transformer import TimeSeriesTransformer
-from .wiki_lstm_network import WikiTimeSeriesLSTMNetworkDaily
