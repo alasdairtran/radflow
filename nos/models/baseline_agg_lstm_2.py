5084d4593e83c19f715acf2e1909468d8415d851
Alasdair Tran <alasdair.tran@gmail.com>
2020-07-27 11:52:32 +1000

    Import h5py

--- nos/models/baseline_agg_lstm_2.py
+++ nos/models/baseline_agg_lstm_2.py
@@ -6,6 +6,7 @@ from collections import Counter, defaultdict
 from copy import deepcopy
 from typing import Any, Dict, List
 
+import h5py
 import numpy as np
 import pandas as pd
 import redis
--- nos/models/nbeats_wiki.py
+++ nos/models/nbeats_wiki.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 from copy import deepcopy
 from typing import Any, Dict, List
 
+import h5py
 import numpy as np
 import pandas as pd
 import redis
