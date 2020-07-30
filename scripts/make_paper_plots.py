ae8c53f467edcdae28f03a9aa165e1f52245c7a6
Alasdair Tran <alasdair.tran@gmail.com>
2020-07-30 23:45:09 +1000

    Add missing edge plot

--- scripts/make_paper_plots.py
+++ scripts/make_paper_plots.py
@@ -47,24 +47,68 @@ def plot_missing_views():
     y2 = get_means(one_hop_ff_00_metrics)
     y3 = get_means(two_hop_ff_00_metrics)
 
-    fig = plt.figure(figsize=(6, 4))
+    fig = plt.figure(figsize=(6, 3))
     ax = plt.subplot(1, 2, 1)
 
-    xs = [0, 0.2, 0.4, 0.6, 0.8]
+    xs = [0, 20, 40, 60, 80]
     ax.errorbar(xs, y1, marker='x', linestyle=':')
     ax.errorbar(xs, y2, marker='x', linestyle='--')
     ax.errorbar(xs, y3, marker='x', linestyle='-')
 
     ax.set_ylabel('SMAPE-7')
-    ax.set_xlabel('Proportion of missing views')
+    ax.set_xlabel('% of missing views')
     ax.legend(['No hops', 'One hop', 'Two hops'])
 
     ax = plt.subplot(1, 2, 2)
     ax.set_ylabel('SMAPE-28')
-    ax.set_xlabel('Proportion of missing views')
+    ax.set_xlabel('% of missing views')
 
     fig.tight_layout()
-    fig.savefig('figures/vevo_missing_views.pdf')
+    fig.savefig('figures/missing_views.pdf')
+
+
+def plot_missing_edges():
+    data = 'vevo'
+    no_hops_ff_00_metrics = [f'expt/missing/edges/{data}/no_hops_ff_00/00/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/no_hops_ff_00/20/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/no_hops_ff_00/40/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/no_hops_ff_00/60/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/no_hops_ff_00/80/serialization/evaluate-metrics.json']
+
+    one_hop_ff_00_metrics = [f'expt/missing/edges/{data}/one_hop_ff_00/00/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/one_hop_ff_00/20/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/one_hop_ff_00/40/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/one_hop_ff_00/60/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/one_hop_ff_00/80/serialization/evaluate-metrics.json']
+
+    two_hop_ff_00_metrics = [f'expt/missing/edges/{data}/two_hops_ff_00/00/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/two_hops_ff_00/20/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/two_hops_ff_00/40/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/two_hops_ff_00/60/serialization/evaluate-metrics.json',
+                             f'expt/missing/edges/{data}/two_hops_ff_00/80/serialization/evaluate-metrics.json']
+
+    y1 = get_means(no_hops_ff_00_metrics)
+    y2 = get_means(one_hop_ff_00_metrics)
+    y3 = get_means(two_hop_ff_00_metrics)
+
+    fig = plt.figure(figsize=(6, 3))
+    ax = plt.subplot(1, 2, 1)
+
+    xs = [0, 20, 40, 60, 80]
+    ax.errorbar(xs, y1, marker=None, linestyle=':')
+    ax.errorbar(xs, y2, marker='x', linestyle='--')
+    ax.errorbar(xs, y3, marker='x', linestyle='-')
+
+    ax.set_ylabel('SMAPE-7')
+    ax.set_xlabel('% of missing edges')
+    ax.legend(['No hops', 'One hop', 'Two hops'], bbox_to_anchor=(0.51, 0.9))
+
+    ax = plt.subplot(1, 2, 2)
+    ax.set_ylabel('SMAPE-28')
+    ax.set_xlabel('% of missing edges')
+
+    fig.tight_layout()
+    fig.savefig('figures/missing_edges.pdf')
 
 
 def generate_word_cloud(seed):
@@ -91,6 +135,7 @@ def generate_word_cloud(seed):
 
 def main():
     plot_missing_views()
+    plot_missing_edges()
 
     generate_word_cloud('global_health')
     generate_word_cloud('programming_languages')
