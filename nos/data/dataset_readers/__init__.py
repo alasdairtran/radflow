ee4cff0e4944d3efdcdbf8d7143ec4424285ebf2
Alasdair Tran <alasdair.tran@gmail.com>
2020-08-04 19:23:23 +1000

    Delete old dataset readers

--- nos/data/dataset_readers/__init__.py
+++ nos/data/dataset_readers/__init__.py
@@ -1,5 +1,1 @@
-from .subwiki import SubWikiReader
 from .subwiki_network import SubWikivNetworkReader
-from .vevo import VevovReader
-from .vevo_network import VevovNetworkReader
-from .wiki_network import WikiNetworkReader
